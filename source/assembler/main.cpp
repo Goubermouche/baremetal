2262aef82618ed41526da3f1f6c66192ece15b15
Šimon Tupý <simontupy64@gmail.com>
2024-05-08 22:49:30 +0200

    Working segmented array (inefficient memory).

--- source/assembler/main.cpp
+++ source/assembler/main.cpp
@@ -9,8 +9,13 @@
 using namespace utility::types;
 
 auto compare_vector_and_segmented(const std::vector<i32>& vec, utility::segmented_array<i32>& arr) {
-	// TODO: size
 	u64 index = 0;
+
+	if(vec.size() != arr.size()) {
+		utility::iterator_print(vec.begin(), vec.end());
+		utility::iterator_print(arr.begin(), arr.end());
+		return false;
+	}
 
 	for(const i32 a : arr) {
 		if(vec[index++] != a) {
@@ -25,37 +30,48 @@ auto compare_vector_and_segmented(const std::vector<i32>& vec, utility::segmente
 
 void test_insert() {
 	constexpr i32 iteration_count = 10;
-	constexpr u64 segment_size = 10;
 
-	for(i32 i = 0; i < iteration_count + 1; ++i) {
-		for(i32 j = 0; j < iteration_count; ++j) {
-			// std baseline
-			std::vector<i32> std_destination(iteration_count);
-			std::vector<i32> std_source(j, 1000);
+	i32 i = 6;
+	i32 j = 3;
+	i32 k = 6;
 
-			std::iota(std_destination.begin(), std_destination.end(), 0);
-			std_destination.insert(std_destination.begin() + i, std_source.begin(), std_source.end());
+	//i32 i = 6;
+	//i32 j = 5;
+	//i32 k = 6;
 
-			// utility
-			utility::segmented_array<i32> utility_destination(segment_size);
-			utility::segmented_array<i32> utility_source(segment_size);
+	//for(i32 i = 0; i < iteration_count + 1; ++i) {
+	//	for(i32 j = 0; j < iteration_count; ++j) {
+	//		for(i32 k = 1; k < iteration_count; ++k) {
+				// std baseline
+				std::vector<i32> std_destination(iteration_count);
+				std::vector<i32> std_source(j, 1000);
 
-			for(i32 k = 0; k < j; ++k) {
-				utility_source.push_back(1000);
-			}
+				std::iota(std_destination.begin(), std_destination.end(), 0);
+				std_destination.insert(std_destination.begin() + i, std_source.begin(), std_source.end());
 
-			for(i32 k = 0; k < iteration_count; ++k) {
-				utility_destination.push_back(k);
-			}
+				// utility
+				utility::segmented_array<i32> utility_destination(k);
+				utility::segmented_array<i32> utility_source(k);
 
-			utility_destination.insert(utility_destination.begin() + i, utility_source.begin(), utility_source.end());
+				for(i32 l = 0; l < j; ++l) {
+					utility_source.push_back(1000);
+				}
 
-			if(!compare_vector_and_segmented(std_destination, utility_destination)) {
-				std::cout << '{' << i << ", " << j << "}\n";
-				DEBUG_BREAK();
-			}
-		}
-	}
+				for(i32 l = 0; l < iteration_count; ++l) {
+					utility_destination.push_back(l);
+				}
+
+				utility::iterator_print(utility_destination.begin(), utility_destination.end());
+				utility_destination.insert(utility_destination.begin() + i, utility_source.begin(), utility_source.end());
+				utility::iterator_print(utility_destination.begin(), utility_destination.end());
+
+				if(!compare_vector_and_segmented(std_destination, utility_destination)) {
+					std::cout << '{' << i << ", " << j << ", " << k << "}\n";
+					//DEBUG_BREAK();
+				}
+	//		}
+	//	}
+	//}
 }
 
 int main() {
